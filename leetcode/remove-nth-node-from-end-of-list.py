121a2a2c21eb57cf4ea9c88ed669d4301d60f220
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-08-17 20:10:50 -0400

    Update solution for remove-nth-node-from-end-of-list

--- leetcode/remove-nth-node-from-end-of-list.py
+++ leetcode/remove-nth-node-from-end-of-list.py
@@ -5,16 +5,19 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        dummyNode = ListNode(0, head)
-        left, right = dummyNode, head
-
-        for _ in range(n):
+        # creating a dummy Node here is super helpful for dealing with edge cases
+        dummy = ListNode(0, head)
+        left, right = dummy, dummy
+        
+        # create a gap of n nodes between left and right pointers
+        for _ in range(n + 1):
             right = right.next
         
+        # iterate and update until right pointer reaches end of LL
         while right:
-            left = left.next
-            right = right.next
+            left, right = left.next, right.next
         
+        # the node we want to remove is the node right next to left ptr
         left.next = left.next.next
-
-        return dummyNode.next
+    
+        return dummy.next
