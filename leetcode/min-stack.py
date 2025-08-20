1ea3d2ba7185e153ca2d506c955edf41f322c1d2
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-08-19 22:19:46 -0400

    Update solution for min-stack

--- leetcode/min-stack.py
+++ leetcode/min-stack.py
@@ -5,13 +5,11 @@ class MinStack:
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        if not self.min_stack:
-            self.min_stack.append(val)
+
+        if self.min_stack and self.min_stack[-1] < val:
+            self.min_stack.append(self.min_stack[-1])
         else:
-            if self.min_stack[-1] > val:
-                self.min_stack.append(val)
-            else:
-                self.min_stack.append(self.min_stack[-1])
+            self.min_stack.append(val)
 
     def pop(self) -> None:
         self.stack.pop()
