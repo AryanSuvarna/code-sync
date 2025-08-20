00be8584749002889a6a7b025f2fd4146b922b9d
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-08-20 13:27:45 -0400

    Update solution for time-needed-to-buy-tickets

--- leetcode/time-needed-to-buy-tickets.py
+++ leetcode/time-needed-to-buy-tickets.py
@@ -1,16 +1,11 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
         time_taken = 0
-        # before k
-        for i in range(k):
-            time_taken += min(tickets[k], tickets[i])
-        # at k
-        time_taken += tickets[k]
         
-        # after k
-        for j in range(k + 1, len(tickets)):
-            if tickets[k] > tickets[j]:
-                time_taken += tickets[j]
+        for idx, tickets_needed in enumerate(tickets):
+            if idx <= k:
+                time_taken += min(tickets[k], tickets_needed)
             else:
-                time_taken += tickets[k] - 1
+                time_taken += min(tickets_needed, tickets[k] - 1)
+
         return time_taken
