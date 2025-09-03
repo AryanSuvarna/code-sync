55753a46d3b83afb8bf09e625b434d0388cfb01a
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-09-02 23:42:46 -0400

    Update solution for find-the-number-of-ways-to-place-people-i

--- leetcode/find-the-number-of-ways-to-place-people-i.py
+++ leetcode/find-the-number-of-ways-to-place-people-i.py
@@ -4,16 +4,16 @@ class Solution:
         points.sort(key=lambda p: (p[0], -p[1]))
         ans = 0
 
-        for i in range(len(points)):
-            yi = points[i][1]
-            maxY = float("-inf")
+        for a in range(len(points)):
+            A_y = points[a][1]
+            prev = float("-inf")
             # Explore candidate Bs
-            for j in range(i + 1, len(points)):
-                yj = points[j][1]
+            for b in range(a + 1, len(points)):
+                B_y = points[b][1]
                 # Condition: A is upper-left (yi >= yj)
                 # And ensure no point has been inside rectangle: yj > maxY
-                if yi >= yj > maxY:
+                if A_y >= B_y > prev:
                     ans += 1
-                    maxY = yj
+                    prev = B_y
 
         return ans
