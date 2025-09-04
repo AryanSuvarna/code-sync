3d73128a586cb361ffc8d91f7fc57ea46a978672
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-09-03 20:45:46 -0400

    Update solution for find-the-number-of-ways-to-place-people-ii

--- leetcode/find-the-number-of-ways-to-place-people-ii.py
+++ leetcode/find-the-number-of-ways-to-place-people-ii.py
@@ -1,14 +1,18 @@
 class Solution:
     def numberOfPairs(self, points: List[List[int]]) -> int:
+        # sort the points
         sorted_points = sorted(points, key=lambda p:(p[0], -p[1]))
         count = 0
 
         for a in range(len(sorted_points)):
+            # grab Alice's y coord
             A_y = sorted_points[a][1]
             prev = float("-inf")
             for b in range(a + 1, len(sorted_points)):
+                # grab Bob's y coord
                 B_y = sorted_points[b][1]
 
+                # if Alice's and Bob's y-coord is greater than prev, this means there is no person in between them
                 if A_y >= B_y > prev:
                     count += 1
                     prev = B_y
