75c2d402d17edd3be19173e681a35ba51013ed73
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-09-04 23:43:13 -0400

    Update solution for same-tree

--- leetcode/same-tree.py
+++ leetcode/same-tree.py
@@ -6,22 +6,11 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        
+        # returns True/False
         def dfs(root_1, root_2):
             if not root_1 and not root_2:
                 return True
-            # root_1 and root_2 structurally differ from each other somehow 
-            elif not root_1 or not root_2:
-                return False
-            else:
-                # compute their branches recursively
-                left_branches = dfs(root_1.left, root_2.left)
-                right_branches = dfs(root_1.right, root_2.right)
-
-                # if the branches are the same, check the current values
-                if left_branches and right_branches and root_1.val == root_2.val:
-                    return True
-                else:
-                    return False
-                
+            if root_1 and root_2 and root_1.val == root_2.val:
+                return (dfs(root_1.left, root_2.left) and dfs(root_1.right, root_2.right))
+            return False               
         return dfs(p, q)
