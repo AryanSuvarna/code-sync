69d571841a554a587e8911caf353f7102ee7083e
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-09-06 12:24:34 -0400

    Update solution for minimum-operations-to-make-array-elements-zero

--- leetcode/minimum-operations-to-make-array-elements-zero.py
+++ leetcode/minimum-operations-to-make-array-elements-zero.py
@@ -1,16 +1,22 @@
 class Solution:
-    def get(self, num: int) -> int:
-        i = 1
-        base = 1
-        cnt = 0
-        while base <= num:
-            cnt += ((i + 1) // 2) * (min(base * 2 - 1, num) - base + 1)
-            i += 1
-            base *= 2
-        return cnt
-
     def minOperations(self, queries: List[List[int]]) -> int:
         res = 0
-        for q in queries:
-            res += (self.get(q[1]) - self.get(q[0] - 1) + 1) // 2
+        
+        def get_operations(x):
+            bits = 1
+            base = 1
+            total_ops = 0
+
+            while base <= x:
+                # operations required = (total amount of numbers you can make with bits) * (# of ops required to transform each number with x bits to 0)
+                total_ops += (min(base * 2 - 1, x) - base + 1) * ((bits + 1) // 2)
+                bits += 1
+                base *= 2
+            return total_ops
+        
+        # go thru every query
+        for l, r in queries:
+            # calculate the number of operations required for each interval
+            res += (get_operations(r) - get_operations(l - 1) + 1) // 2
+        
         return res
