3a749079acc3a73a7d37b55a87f5f5a528de40f6
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-12-15 17:41:52 -0500

    Update solution for search-a-2d-matrix

--- leetcode/search-a-2d-matrix.py
+++ leetcode/search-a-2d-matrix.py
@@ -1,35 +1,34 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        # first, we determine which is a possible row that target can reside in
-        left_row, right_row = 0, len(matrix) - 1
-        m_row = 0
+        l_row, r_row = 0, len(matrix) - 1
+        m_row = l_row
 
-        while left_row <= right_row:
-            m_row = (left_row + right_row) // 2
+        # find ideal row for target
+        while l_row <= r_row:
+            m_row = (l_row + r_row) // 2
 
             if matrix[m_row][0] > target:
-                right_row = m_row - 1
+                r_row = m_row - 1
             elif matrix[m_row][-1] < target:
-                left_row = m_row + 1
+                l_row = m_row + 1
             else:
                 break
         
-        # check how we broke out of the while loop:
-        if left_row > right_row:
+        # return False if no ideal row is found for the target
+        if not (l_row <= r_row):
             return False
         
-        # now that we found a suitable row, we check if this row contains target
-        row_to_check = matrix[m_row]
-        l, r = 0, len(row_to_check) - 1
+        l, r = 0, len(matrix[m_row]) - 1
 
+        # regular binary search
         while l <= r:
             m = (l + r) // 2
 
-            if target == row_to_check[m]:
-                return True
-            elif target < row_to_check[m]:
+            if matrix[m_row][m] < target:
+                l = m + 1
+            elif matrix[m_row][m] > target:
                 r = m - 1
             else:
-                l = m + 1
+                return True
         
         return False
