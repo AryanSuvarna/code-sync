4426f3b5bb99aabfa9690bf8ac5a6e5cc2508ac4
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-12-17 20:43:42 -0500

    Update solution for search-in-rotated-sorted-array

--- leetcode/search-in-rotated-sorted-array.py
+++ leetcode/search-in-rotated-sorted-array.py
@@ -1,30 +1,27 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # problem running in O(log n) is clear indicating that binary search is needed for problem
-
         l, r = 0, len(nums) - 1
 
-        while l <= r:
+        while l <=r:
             m = (l + r) // 2
-            
-            # we found target!
+
             if target == nums[m]:
                 return m
 
-            # we're on left side of split
-            if nums[r] < nums[m]:
-                # within range of (l, m)
-                if target < nums[m] and target >= nums[l]:
+            # left side of split
+            if nums[m] > nums[r]:
+
+                # within range
+                if nums[l] <= target and target < nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
             
-            # we're on right side of split
             else:
-                # within range of (m, r)
-                if target > nums[m] and target <= nums[r]:
+                # within range
+                if nums[m] < target and target <= nums[r]:
                     l = m + 1
                 else:
                     r = m - 1
-
+                
         return -1
