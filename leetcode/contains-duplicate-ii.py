d058c053259ee40c40332786fe0097dbd4ce9ffb
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-12-21 11:53:50 -0500

    Update solution for contains-duplicate-ii

--- leetcode/contains-duplicate-ii.py
+++ leetcode/contains-duplicate-ii.py
@@ -4,16 +4,13 @@ class Solution:
         l = 0
 
         for r in range(len(nums)):
-            # window greater than k
             if r - l > k:
                 window.remove(nums[l])
                 l += 1
             
-            # we found duplicate in window
             if nums[r] in window:
                 return True
             
-            # add this value otherwise
             window.add(nums[r])
-
+            
         return False
