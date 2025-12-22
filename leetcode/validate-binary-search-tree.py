fda604398a1b7d1322302666d0834b8a45797ae3
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-12-22 12:34:51 -0500

    Update solution for validate-binary-search-tree

--- leetcode/validate-binary-search-tree.py
+++ leetcode/validate-binary-search-tree.py
@@ -6,20 +6,18 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        
+
         def dfs(root, left, right):
             # base case
             if not root:
                 return True
 
-            # root value has to be in between these 2 values
             if not (left < root.val < right):
                 return False
             
-            # make sure the left and right branches are valid as well
-            left_branch = dfs(root.left, left, root.val)
-            right_branch = dfs(root.right, root.val, right)
+            left_res = dfs(root.left, left, root.val)
+            right_res = dfs(root.right, root.val, right)
 
-            return left_branch and right_branch
-        
+            return left_res and right_res    
+
         return dfs(root, float("-inf"), float("inf"))
