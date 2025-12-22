99378a73d20f0185bbbf7f1b990efcf1651cbb32
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-12-22 13:11:49 -0500

    Update solution for longest-repeating-character-replacement

--- leetcode/longest-repeating-character-replacement.py
+++ leetcode/longest-repeating-character-replacement.py
@@ -1,21 +1,18 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # initialize these variables
-        l, longest, most_frequent_character = 0, 0, 0
-        letter_map = defaultdict(int)
+        longest = 0
+        l = 0
+        letter_count = defaultdict(int)
 
         for r in range(len(s)):
-            # append the new character to map
-            letter_map[s[r]] += 1
-            # update the most_frequent_character only when we increase the window size
-            most_frequent_character = max(most_frequent_character, letter_map[s[r]])
-
-            # valid window: window length - most_frequent <= k
-            while (r - l + 1) - most_frequent_character > k:
-                letter_map[s[l]] -= 1
+            # append the new char to letter count first
+            letter_count[s[r]] += 1
+            
+            # check if this is true: (window_size - largest_count > k)
+            while (r - l + 1) - max(letter_count.values()) > k:
+                letter_count[s[l]] -= 1
                 l += 1
             
-            # update the count of longest window, if possible
             longest = max(longest, r - l + 1)
-        
+
         return longest
