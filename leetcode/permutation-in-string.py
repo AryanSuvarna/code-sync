f3715bc977324bd8d9705f88b41313fa1b88fceb
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-12-23 12:32:58 -0500

    Update solution for permutation-in-string

--- leetcode/permutation-in-string.py
+++ leetcode/permutation-in-string.py
@@ -1,37 +1,33 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # length of s2 cannot be less than length of s1
-        if len(s2) < len(s1):
+        # permutation cannot exist is len(s1) is greater than len(s2)
+        if len(s1) > len(s2):
             return False
         
-        # get the letter count of s1
+        # get the letter hash map for s1
         s1_map = defaultdict(int)
 
-        for i in range(len(s1)):
-            s1_map[s1[i]] += 1
+        for c in s1:
+            s1_map[c] += 1
 
-        window_size = len(s1) 
-
-        # initialize the rolling hash map of s2
+        window_size = len(s1)
+        l = 0
         s2_map = defaultdict(int)
 
-        for j in range(window_size):
-            s2_map[s2[j]] += 1
+        for r in range(len(s2)):
+            # check if window is too large
+            if (r - l + 1) > window_size:
+                s2_map[s2[l]] -= 1
+                # pop this if the count is 0 ever
+                if s2_map[s2[l]] == 0:
+                    s2_map.pop(s2[l])
+                l += 1
 
-        # then we are going to compare the 2 maps by having a sliding window with len(s1) size
-        l = 0
-
-        for r in range(window_size, len(s2)):
-            if s2_map == s1_map:
-                return True
-            
-            # add the next element
+            # append rightmost value
             s2_map[s2[r]] += 1
 
-            # remove the left element
-            s2_map[s2[l]] -= 1
-            if s2_map[s2[l]] == 0:
-                s2_map.pop(s2[l])
-            l += 1
-    
-        return s1_map == s2_map
+            # check if the maps are the same values
+            if s2_map == s1_map:
+                return True
+
+        return False
