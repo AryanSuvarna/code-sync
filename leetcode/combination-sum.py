25bc9b78e29589f9bc6fd97bc21c179fea1f5a36
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-12-25 20:58:24 -0500

    Update solution for combination-sum

--- leetcode/combination-sum.py
+++ leetcode/combination-sum.py
@@ -10,15 +10,16 @@ class Solution:
             elif total == target:
                 res.append(curr_combo.copy())
                 return
-
+            
+            # keep adding current idx value to total to get as close to target as possible
             total += candidates[idx]
             curr_combo.append(candidates[idx])
             backtrack(idx, total)
 
+            # now we remove the current value and see if we can append the next value
             total -= candidates[idx]
             curr_combo.pop()
             backtrack(idx + 1, total)
         
         backtrack(0, 0)
-
         return res
