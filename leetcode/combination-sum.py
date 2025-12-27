f9914d0011364e5887888f247179826986f64ca4
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-12-27 13:34:24 -0500

    Update solution for combination-sum

--- leetcode/combination-sum.py
+++ leetcode/combination-sum.py
@@ -1,25 +1,21 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
+        combo = []
+        candidates.sort()
 
-        curr_combo = []
-
-        def backtrack(idx, total):
-            if total > target or idx >= len(candidates):
-                return
-            elif total == target:
-                res.append(curr_combo.copy())
+        def backtrack(i, total):
+            if total == target:
+                res.append(combo.copy())
                 return
             
-            # keep adding current idx value to total to get as close to target as possible
-            total += candidates[idx]
-            curr_combo.append(candidates[idx])
-            backtrack(idx, total)
-
-            # now we remove the current value and see if we can append the next value
-            total -= candidates[idx]
-            curr_combo.pop()
-            backtrack(idx + 1, total)
-        
+            for j in range(i, len(candidates)):
+                if total + candidates[j] > target:
+                    return
+                
+                combo.append(candidates[j])
+                backtrack(j, total + candidates[j])
+                combo.pop()
+            
         backtrack(0, 0)
         return res
