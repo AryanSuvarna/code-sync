c3309d2587e2c9ee37e124093fca2f871a4d2d15
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-12-27 11:31:35 -0500

    Update solution for combination-sum-ii

--- leetcode/combination-sum-ii.py
+++ leetcode/combination-sum-ii.py
@@ -1,29 +1,26 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
+        candidates.sort()
 
-        subset = []
-        def backtrack(i, total):
+        path = []
+        def backtrack(curr_idx, total):
             if total == target:
-                res.append(subset.copy())
-                return
-            if i >= len(candidates) or total > target:
+                res.append(path.copy())
                 return
             
-            # include curr indx
-            total += candidates[i]
-            subset.append(candidates[i])
-            backtrack(i + 1, total)
+            for i in range(curr_idx, len(candidates)):
+                # we've seen this value already
+                if i > curr_idx and candidates[i] == candidates[i - 1]:
+                    continue
+                
+                # early break if the candidate value will be greater than target
+                if total + candidates[i] > target:
+                    break
 
-            # exclude curr idx
-            total -= candidates[i]
-            subset.pop()
-            # skip duplicate numbers since we already processed this value above
-            while i + 1 < len(candidates) and candidates[i] == candidates[i + 1]:
-                i += 1
-            backtrack(i + 1, total)
-
-        # candidates list must be sorted so we can avoid dupes
-        candidates.sort()
-        backtrack(0,0)
+                path.append(candidates[i])
+                backtrack(i + 1, total + candidates[i])
+                path.pop()
+        
+        backtrack(0, 0)
         return res
