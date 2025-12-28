4fa1d1aac0a7d4782c4cae44cd70c5642d7bedf6
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2025-12-27 22:12:26 -0500

    Update solution for permutations

--- leetcode/permutations.py
+++ leetcode/permutations.py
@@ -1,18 +1,29 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        # RECURSIVE SOLUTION
-        # base case
-        if len(nums) == 0:
-            return [[]]
-        
-        # self calling self.permute till you get to base case and build upwards
-        perms = self.permute(nums[1:])
         res = []
-        for p in perms:
-            # add the element that you excluded (nums[0]), to every possible position (start, middles, end)
-            for i in range(len(p) + 1):
-                p_copy = p.copy()
-                p_copy.insert(i, nums[0])
-                res.append(p_copy)
-        
+
+        def backtrack(num_used, perm):
+            # base case: we made a permutation!
+            if len(perm) == len(nums):
+                res.append(perm.copy())
+                return
+            
+            for num in nums:
+                # skip this num as it's in permutation already
+                if num in num_used:
+                    continue
+                
+                # include num
+                num_used.add(num)
+                perm.append(num)
+
+                # explore with new addition...
+                backtrack(num_used, perm)
+
+                # exclude
+                num_used.remove(num)
+                perm.pop()
+            
+        backtrack(set(), [])
+
         return res
