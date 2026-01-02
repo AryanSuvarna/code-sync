6a52512319d1c66eef00f6d55ba4508353f0766b
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-01-02 12:47:10 -0500

    Update solution for house-robber

--- leetcode/house-robber.py
+++ leetcode/house-robber.py
@@ -1,24 +1,27 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # memoize past computations
         memo = {}
 
-        def dfs(i):
-            # base case: index is out of bounds
+        def dp(i):
+            # base case: end of list
             if i >= len(nums):
                 return 0
-            
-            # check if we've already computed the largest robbing value from index i
+
             if i in memo:
                 return memo[i]
             
-            # 2 scenarios: rob house i and move on OR skip house i and check next
-            rob = nums[i] + dfs(i + 2)
-            skip = dfs(i + 1)
+            # rob the current house and go 2 houses down
+            rob = nums[i] + dp(i + 2)
 
-            # take the larger of the 2 options and memoize it
+            # skip this house and go the next house
+            skip = dp(i + 1)
+
             memo[i] = max(rob, skip)
 
+            # return the scenario that returns the highest value
             return memo[i]
         
-        res = dfs(0)
+        res = dp(0)
+
         return res
