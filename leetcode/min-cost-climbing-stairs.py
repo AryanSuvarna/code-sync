f222a4f6044e0a0fb055bf16bec1aa6eec03768c
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-01-07 14:04:25 -0500

    Update solution for min-cost-climbing-stairs

--- leetcode/min-cost-climbing-stairs.py
+++ leetcode/min-cost-climbing-stairs.py
@@ -3,13 +3,17 @@ class Solution:
         memo = {}
 
         def dfs(i):
-            if i >= len(cost):
-                return 0
             if i in memo:
                 return memo[i]
+
+            # out of bounds error
+            if i >= len(cost):
+                return 0
             
+            # current cost + cost from next step and 2 steps after
             memo[i] = cost[i] + min(dfs(i + 1), dfs(i + 2))
 
             return memo[i]
         
+        # take the min from step 0 or step 1
         return min(dfs(0), dfs(1))
