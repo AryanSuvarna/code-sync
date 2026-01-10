ad4d505bb4f8eb561b72fde5996b6e8ff8d743bb
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-01-09 19:27:50 -0500

    Update solution for coin-change

--- leetcode/coin-change.py
+++ leetcode/coin-change.py
@@ -1,15 +1,20 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        # simple base case; early exit
         if amount == 0:
             return 0
         
+        # dp memoization, store min coins needed for each result
         memo = {}
 
         def dp(total):
+            # total in memo
             if total in memo:
                 return memo[total]
+            # we can get to total
             if total == 0:
                 return 0
+            # we overshot
             if total < 0:
                 return float("inf")
             
@@ -19,7 +24,8 @@ class Solution:
             
             memo[total] = res
 
-            return memo[total]
+            return res
         
         ans = dp(amount)
+
         return -1 if ans == float("inf") else ans
