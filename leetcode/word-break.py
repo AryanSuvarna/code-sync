6d2d3eeea1800fa9c351bb5be762a662fa8f1498
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-01-09 19:44:27 -0500

    Update solution for word-break

--- leetcode/word-break.py
+++ leetcode/word-break.py
@@ -1,23 +1,16 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # BOTTOM UP APPROACH
-
-        # our DP array
+        # dp array
         dp = [False for i in range(len(s) + 1)]
-
-        # base case: if we ever reach the end of the string, we have successfully broken strings
+        # index that is next to end of string means that we iterated thru whole string successfully
         dp[len(s)] = True
-    
-        for i in range(len(s) - 1, -1, -1):
-            # checking if from ith index, we can create one of the words
+
+        for i in range(len(s) - 1, -1, -1):           
             for word in wordDict:
-                # check if the word can be made starting for index i
-                if s[i : i + len(word)] == word:
-                    # if possible, use the subproblems we already solved to determine if word can be segmented
+                if i + len(word) <= len(s) and s[i : i + len(word)] == word:
                     dp[i] = dp[i + len(word)]
-                # break early if we already found a suitable word
+                
                 if dp[i]:
                     break
-        
-        # print(dp)
+
         return dp[0]
