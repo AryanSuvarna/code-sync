0f174744511e9ebb17c44efdc8ee9ab3b4ecc7ef
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-01-11 20:12:51 -0500

    Update solution for longest-increasing-subsequence

--- leetcode/longest-increasing-subsequence.py
+++ leetcode/longest-increasing-subsequence.py
@@ -1,16 +1,11 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        # BOTTOM UP SOLUTION
-        
-        # dp array keeping track of the largest subsequence we can create at each index
-        LIS = [1] * len(nums)
+        # bottom up
+        LIS = [1 for i in range(len(nums))]
 
-        # iterate backwards
         for i in range(len(nums) - 1, -1, -1):
-            # check every position after index i
             for j in range(i + 1, len(nums)):
                 if nums[i] < nums[j]:
                     LIS[i] = max(LIS[i], 1 + LIS[j])
-        
-        # return the largest subsquence in the list
+
         return max(LIS)
