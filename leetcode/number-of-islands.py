cb9d49ed70503453073af9467931d8d7395c1232
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-01-14 22:17:20 -0500

    Update solution for number-of-islands

--- leetcode/number-of-islands.py
+++ leetcode/number-of-islands.py
@@ -1,29 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        ROWS = len(grid)
-        COLS = len(grid[0])
+        if not grid:
+            return 0
         
-        def dfs(row, col):
-            # edge cases 
-            if row < 0 or col < 0 or row >= ROWS or col >= COLS or grid[row][col] == '0':
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            # check edge cases
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] == "0":
                 return
             
-            # change land to water so we dont visit it ever again 
-            grid[row][col] = '0'
-            
-            # check in all directions
-            dfs(row + 1, col)
-            dfs(row - 1, col)
-            dfs(row, col + 1)
-            dfs(row, col - 1)
+            # if we dont pass any of the edge cases, then the curr position is land
+            # we will set it to 0 to mark it as "visited" (we never look at 0 spots ever)
+            grid[r][c] = "0"
 
-            return
-
-        numIslands = 0
+            # search grid now in all 4 directions
+            for x, y in directions:
+                dfs(r + x, c + y)
+    
+        # go thru every position till we see land. once we do, run dfs on that position
+        count = 0
         for r in range(ROWS):
             for c in range(COLS):
-                if grid[r][c] == '1':
+                if grid[r][c] == "1":
+                    count += 1
                     dfs(r, c)
-                    numIslands += 1
-        
-        return numIslands
+
+        return count
