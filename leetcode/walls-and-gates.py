5eb84a6c747774df6aba959c5e7acf8d2777b239
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-01-16 20:02:57 -0500

    Add solution for walls-and-gates

---
+++ leetcode/walls-and-gates.py
@@ -0,0 +1,36 @@
+class Solution:
+    def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        """
+        Do not return anything, modify rooms in-place instead.
+        """
+        ROWS, COLS = len(rooms), len(rooms[0])
+        visited = set()
+        q = deque()
+
+        def add_room(r, c):
+            if r < 0 or c < 0 or c >= COLS or r >= ROWS or (r, c) in visited or rooms[r][c] == -1:
+                return
+            
+            visited.add((r, c))
+            q.append((r, c))
+        
+        # add all the treasures first in queue
+        for r in range(ROWS):
+            for c in range(COLS):
+                if rooms[r][c] == 0:
+                    q.append((r, c))
+                    visited.add((r, c))
+        
+        dist = 0
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+
+                rooms[r][c] = dist
+
+                add_room(r + 1, c)
+                add_room(r - 1, c)
+                add_room(r, c + 1)
+                add_room(r, c - 1)
+            
+            dist += 1
