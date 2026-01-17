24b846ec9ea83cd2997a213bbe10c248c48e55d1
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-01-17 16:26:07 -0500

    Update solution for walls-and-gates

--- leetcode/walls-and-gates.py
+++ leetcode/walls-and-gates.py
@@ -4,26 +4,27 @@ class Solution:
         Do not return anything, modify rooms in-place instead.
         """
         ROWS, COLS = len(rooms), len(rooms[0])
-        visited = set()
-        q = deque()
+        q = deque() # bfs queue
+        visited = set() # keep track of rooms visited
 
         def add_room(r, c):
-            if r < 0 or c < 0 or c >= COLS or r >= ROWS or (r, c) in visited or rooms[r][c] == -1:
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited or rooms[r][c] == -1:
                 return
             
             visited.add((r, c))
-            q.append((r, c))
-        
-        # add all the treasures first in queue
+            q.append([r, c])
+
+        # first we will add all gates to the queue first
         for r in range(ROWS):
             for c in range(COLS):
                 if rooms[r][c] == 0:
-                    q.append((r, c))
+                    q.append([r, c])
                     visited.add((r, c))
-        
+
+        # iterate on the queue
         dist = 0
         while q:
-            for i in range(len(q)):
+            for _ in range(len(q)):
                 r, c = q.popleft()
 
                 rooms[r][c] = dist
