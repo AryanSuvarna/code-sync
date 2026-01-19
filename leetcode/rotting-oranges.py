7bf1d8eacb28fbc190f5f39495a0a21c3f44d4df
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-01-19 13:49:42 -0500

    Update solution for rotting-oranges

--- leetcode/rotting-oranges.py
+++ leetcode/rotting-oranges.py
@@ -1,29 +1,40 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
+        # queue will be needed to perform BFS
         q = deque()
-        fresh = 0
+        fresh_count = 0
+        time_count = 0
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-        # initialize queue and count fresh oranges
+        def rot_orange(r, c):
+            nonlocal fresh_count
+            # our of bounds edge case and case where 
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or grid[r][c] != 1:
+                return
+            
+            # we have fresh orange, so rot it and add rotten orange to queue
+            grid[r][c] = 2
+            q.append([r, c])
+            fresh_count -= 1
+            
+
+        # we find all oranges, rotten and fresh
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == 2:
-                    q.append((r, c))
+                    q.append([r,c])
                 elif grid[r][c] == 1:
-                    fresh += 1
-
-        time = 0
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
-
-        while q and fresh > 0:
+                    fresh_count += 1
+        
+        # go thru the queue
+        while q and fresh_count > 0:
             for _ in range(len(q)):
                 r, c = q.popleft()
-                for dr, dc in directions:
-                    nr, nc = r + dr, c + dc
-                    if 0 <= nr < ROWS and 0 <= nc < COLS and grid[nr][nc] == 1:
-                        grid[nr][nc] = 2
-                        fresh -= 1
-                        q.append((nr, nc))
-            time += 1
 
-        return time if fresh == 0 else -1
+                for dr, dc in directions:
+                    rot_orange(r + dr, c + dc)
+            
+            time_count += 1
+
+        return time_count if fresh_count == 0 else -1
