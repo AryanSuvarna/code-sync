e8714bd5a87539a96b0317f9ef9f4f845479bed8
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-01-22 21:43:46 -0500

    Update solution for surrounded-regions

--- leetcode/surrounded-regions.py
+++ leetcode/surrounded-regions.py
@@ -6,20 +6,18 @@ class Solution:
         ROWS, COLS = len(board), len(board[0])
 
         def dfs(r, c):
-            #  if cell is out of bounds, return False
             if r < 0 or c < 0 or r >= ROWS or c >= COLS:
-                return 
+                return
             if board[r][c] != 'O':
                 return
-            
-            # if we find a 'O' cell that is connected to the border, we mark that as B for boundary cell and try to see if there is more cells like this that is connected
+
             board[r][c] = 'B'
             dfs(r - 1, c)
             dfs(r + 1, c)
             dfs(r, c - 1)
             dfs(r, c + 1)
 
-        # find all 'O' cells that are connected to border and then run dfs
+        # find all 'O' cells that are touching the border, then run dfs
         for r in range(ROWS):
             dfs(r, 0)
             dfs(r, COLS - 1)
@@ -28,7 +26,6 @@ class Solution:
             dfs(0, c)
             dfs(ROWS - 1, c)
         
-
         # now we mark all 'O' as 'X' and the 'B' cells get set back to 'O'
         for r in range(ROWS):
             for c in range(COLS):
