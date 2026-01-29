428c255b9c00b8cffe5803c5bbe68029b160328b
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-01-29 11:17:56 -0500

    Update solution for graph-valid-tree

--- leetcode/graph-valid-tree.py
+++ leetcode/graph-valid-tree.py
@@ -2,39 +2,31 @@ class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         # ARYAN
 
-        # valid tree:
-        #   1. no loops
-        #   2. all nodes must be connected
-
         # create adjacency list
         adj_list = defaultdict(list)
 
         for a, b in edges:
             adj_list[a].append(b)
             adj_list[b].append(a)
-        
-        # visited set will keep count of the nodes that we visit
-        visited = set()
 
+
+        visited = set()
+        
+        # will return true/false
         def dfs(curr, prev):
             # cycle detected
-            if curr in visited:
-                return False
-            
-            # dfs ....
+            if curr in visited: return False
+
             visited.add(curr)
-            # go thru every neighboutr
+            # go thru every neighbour and run dfs
             for neighbour in adj_list[curr]:
-                # make sure neighbour is not previous value
-                # then make sure that the dfs of neighbour doesnt return False
                 if neighbour != prev and not dfs(neighbour, curr):
                     return False
             
-            # return true if we successfully looked at all neighbours
+            # true if all neighbours are valid 
             return True
         
-        # run dfs on node 0 (prev value initially will be -1 as all node values are positive)
         if not dfs(0, -1): return False
 
-        # if length of visited set is same as n, indicates that all nodes are connected
+        # only return True if we have visited all the nodes
         return True if len(visited) == n else False
