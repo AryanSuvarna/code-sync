0b3d885d994d854069b2374ab35f0df72c5d845c
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-02-06 12:09:23 -0500

    Update solution for best-time-to-buy-and-sell-stock-with-cooldown

--- leetcode/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ leetcode/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,28 +1,26 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # this cache will store the results of buying/selling at specific indices
-        # key = (idx, will_buy) val = max_profit
+        # key = (idx, buy/sell), val = max_profit
         dp = {}
 
-        def dfs(i, will_buy):
-            # base case: out of bounds
-            if i >= len(prices):
+        def dfs(idx, buying):
+            # out of bounds case
+            if idx >= len(prices):
                 return 0
-            # we've already computed the max profit when buying/selling at this idx
-            if (i, will_buy) in dp:
-                return dp[(i, will_buy)]
+            # we already cached this value
+            if (idx, buying) in dp:
+                return dp[(idx, buying)]
             
-            if will_buy:
-                # if we buy, we cannot buy anymore. we must change state to sell
-                buy = dfs(i + 1, not will_buy) - prices[i]
-                cooldown = dfs(i + 1, will_buy)
-                dp[(i, will_buy)] = max(buy, cooldown)
+            # cases where we buying or selling the stock
+            if buying:
+                buy = dfs(idx + 1, not buying) - prices[idx] 
+                cooldown = dfs(idx + 1, buying)
+                dp[(idx, buying)] = max(buy, cooldown)
             else:
-                # if we sell, change it back to buying state (negation)
-                sell = dfs(i + 2, not will_buy) + prices[i]
-                cooldown = dfs(i + 1, will_buy)
-                dp[(i, will_buy)] = max(sell, cooldown)
+                sell = dfs(idx + 2, not buying) + prices[idx] 
+                cooldown = dfs(idx + 1, buying)
+                dp[(idx, buying)] = max(sell, cooldown)
             
-            return dp[(i, will_buy)]
-        
+            return dp[(idx, buying)]
+
         return dfs(0, True)
