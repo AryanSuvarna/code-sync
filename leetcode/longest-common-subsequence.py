f6317455139514eefedde273abe7001e19809d7c
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-02-08 11:37:34 -0500

    Update solution for longest-common-subsequence

--- leetcode/longest-common-subsequence.py
+++ leetcode/longest-common-subsequence.py
@@ -1,16 +1,19 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # create a dp 2d array that will act as a cache
-        dp = [[0 for _ in range(len(text2) + 1)] for _ in range(len(text1) + 1)]
+        # BOTTOM UP APPROACH
+        
+        # create a 2d DP array that will track the LCS in each subproblem
+        dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
 
+        # iterate backwards since we are building it bottom up
         for i in range(len(text1) - 1, -1, -1):
             for j in range(len(text2) - 1, -1, -1):
-                # case where the letters match
+                # same character case
                 if text1[i] == text2[j]:
+                    # take the sum of the value that is on the diagonal of curr pos
                     dp[i][j] = 1 + dp[i + 1][j + 1]
-                # get the largest of the right or bottom position from curr
-                # do this to keep track of common letter as we build solution 
                 else:
                     dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
         
+        # return the first position since we're buidlding bottom up
         return dp[0][0]
