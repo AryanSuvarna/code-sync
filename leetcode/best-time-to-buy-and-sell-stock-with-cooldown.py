6dffb2191573cb6d1dcaf4503a268280acbb05ce
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-02-08 12:13:15 -0500

    Update solution for best-time-to-buy-and-sell-stock-with-cooldown

--- leetcode/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ leetcode/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,26 +1,28 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # key = (idx, buy/sell), val = max_profit
+        # DFS
+
+        # dp dictionary to memoize already calculated solution
+        # key = (idx, buying), value = max_profit
         dp = {}
 
-        def dfs(idx, buying):
-            # out of bounds case
-            if idx >= len(prices):
+        def dfs(i, buying):
+            # base case: out of bounds
+            if i >= len(prices):
                 return 0
-            # we already cached this value
-            if (idx, buying) in dp:
-                return dp[(idx, buying)]
+            # base case: already memoized
+            if (i, buying) in dp:
+                return dp[(i, buying)]
             
-            # cases where we buying or selling the stock
+            # buying or selling
+            cooldown = dfs(i + 1, buying)
             if buying:
-                buy = dfs(idx + 1, not buying) - prices[idx] 
-                cooldown = dfs(idx + 1, buying)
-                dp[(idx, buying)] = max(buy, cooldown)
+                buy = dfs(i + 1, not buying) - prices[i]
+                dp[(i, buying)] = max(buy, cooldown)
             else:
-                sell = dfs(idx + 2, not buying) + prices[idx] 
-                cooldown = dfs(idx + 1, buying)
-                dp[(idx, buying)] = max(sell, cooldown)
-            
-            return dp[(idx, buying)]
+                sell = dfs(i + 2, not buying) + prices[i]
+                dp[(i, buying)] = max(sell, cooldown)
 
+            return dp[(i, buying)]
+        
         return dfs(0, True)
