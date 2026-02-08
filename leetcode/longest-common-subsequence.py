686ebadac63691c7786cff12cba839df0fdc590e
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-02-08 11:57:21 -0500

    Update solution for longest-common-subsequence

--- leetcode/longest-common-subsequence.py
+++ leetcode/longest-common-subsequence.py
@@ -1,19 +1,23 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        # BOTTOM UP APPROACH
-        
-        # create a 2d DP array that will track the LCS in each subproblem
-        dp = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
+        # SPACE OPTIMIZED SOLUTION
 
-        # iterate backwards since we are building it bottom up
+        # swap since we will assume text2 is the shorter of the 2 texts
+        if len(text1) < len(text2):
+            text1, text2 = text2, text1
+
+        # init for the 2 1-D arrays
+        prev = [0] * (len(text2) + 1)
+        curr = [0] * (len(text2) + 1)
+
+        # iterate bottom up on the array
         for i in range(len(text1) - 1, -1, -1):
             for j in range(len(text2) - 1, -1, -1):
-                # same character case
                 if text1[i] == text2[j]:
-                    # take the sum of the value that is on the diagonal of curr pos
-                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                    curr[j] = 1 + prev[j + 1]
                 else:
-                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
-        
-        # return the first position since we're buidlding bottom up
-        return dp[0][0]
+                    curr[j] = max(curr[j + 1], prev[j])
+
+            prev, curr = curr, prev
+
+        return prev[0]
