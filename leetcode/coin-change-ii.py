afeccad6d4dd55bed85fa7a3cf227a7eb81a3063
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-02-08 12:55:39 -0500

    Update solution for coin-change-ii

--- leetcode/coin-change-ii.py
+++ leetcode/coin-change-ii.py
@@ -4,13 +4,18 @@ class Solution:
         coins.sort()
         dp = [[0] * (amount + 1) for _ in range(n + 1)]
 
+        # set all positions with amount = 0 to 1 (only 1 way to get 0)
         for i in range(n + 1):
             dp[i][0] = 1
 
+        # bottom up: build solution
         for i in range(n - 1, -1, -1):
-            for a in range(amount + 1):
+            # start at 1, since we already know values in a = 0
+            for a in range(1, amount + 1):
                 if a >= coins[i]:
+                    # skip curr coin
                     dp[i][a] = dp[i + 1][a]
+                    # use curr coin
                     dp[i][a] += dp[i][a - coins[i]]
 
         return dp[0][amount]
