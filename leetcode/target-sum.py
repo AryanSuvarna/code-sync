a86e3db0e9a2bcf40e8395a0c65ed0f26b3e2040
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-02-10 16:22:52 -0500

    Update solution for target-sum

--- leetcode/target-sum.py
+++ leetcode/target-sum.py
@@ -1,19 +1,23 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         # defaultdict(int): this will represent the sum-count pairs (key=sum, value=count)
-        dp = [defaultdict(int) for i in range(len(nums) + 1)]
+        dp = defaultdict(int)
 
         # base case
         # this is saying that with 0 elements and a sum of 0, there is one way to compute this
-        dp[0][0] = 1
+        dp[0] = 1
 
         # go thru every every index in nums
         for i in range(len(nums)):
-            for cur_sum, count in dp[i].items():
+            next_dp = defaultdict(int)
+            for cur_sum, count in dp.items():
                 # move to the next row and add the curr nums value
-                dp[i + 1][cur_sum + nums[i]] += count
+                next_dp[cur_sum + nums[i]] += count
                 # move to the next row and remove the 
-                dp[i + 1][cur_sum - nums[i]] += count
+                next_dp[cur_sum - nums[i]] += count
+            
+            # reassign for next loop 
+            dp = next_dp
 
         # we're looking for the value that is stored at when we use all the indices and we reached the target value
-        return dp[len(nums)][target]
+        return dp[target]
