d587f3146c0fc699107f1287759c3ec1251416c9
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-05-22 11:47:04 -0400

    Update solution for climbing-stairs

--- leetcode/climbing-stairs.py
+++ leetcode/climbing-stairs.py
@@ -1,17 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # TOP DOWN DP APPROACH
+        # BOTTOM UP APPROACH (build upwards from base cases)
+
+        # base case
+        one, two = 1, 1
         
-        def dp(n, cache):
-            # for values (0,1,2,3), the # of ways to climb stairs is just the value
-            if n < 4:
-                return n
-            # if in the cache, return wtv value we already calculated
-            if n in cache:
-                return cache[n]
-            
-            # store the value in the cache before returning
-            cache[n] = dp(n - 1, cache) + dp(n - 2, cache)
-            return cache[n]
-        
-        return dp(n, {})
+        # iterate till i = n
+        i = 2
+        while i <= n:
+            next_step = one + two
+            # swap one_step with curr, and two_step with one
+            one, two = next_step, one
+            i += 1
+
+        return one
