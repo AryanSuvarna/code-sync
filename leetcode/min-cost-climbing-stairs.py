477873ecb6f4b514d1c74ca4114033e3cbefffab
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-05-23 13:12:02 -0400

    Update solution for min-cost-climbing-stairs

--- leetcode/min-cost-climbing-stairs.py
+++ leetcode/min-cost-climbing-stairs.py
@@ -1,12 +1,18 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # append 0 to end of list to represent the top of the floor
-        cost.append(0)
+        memo = {}
 
-        # start at 3rd rightmost val because we know the last value will remain the same
-        for i in range(len(cost) - 3, -1, -1):
-            # take the min of curr value with 1 step and curr value with 2 steps and update the position
-            cost[i] = min(cost[i] + cost[i + 1], cost[i] + cost[i + 2])
-        
-        # cost[0] and cost[1] will be updated with the costs to get to top of the floor from position 0 and 1
-        return min(cost[0], cost[1])
+        def dfs(i):
+            # out of bounds case
+            if i >= len(cost):
+                return 0
+            
+            # position i in cached in memo
+            if i in memo:
+                return memo[i]
+            
+            memo[i] = cost[i] + min(dfs(i + 1), dfs(i + 2))
+            
+            return memo[i]
+
+        return min(dfs(0), dfs(1))
