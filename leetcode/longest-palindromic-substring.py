8377da91edcf932a02f0a49f2c33e34d8ab051c9
Aryan Suvarna <61842797+AryanSuvarna@users.noreply.github.com>
2026-05-27 15:34:51 -0400

    Update solution for longest-palindromic-substring

--- leetcode/longest-palindromic-substring.py
+++ leetcode/longest-palindromic-substring.py
@@ -1,24 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res, longest = "", 0
+        res, res_len = "", 0
 
-        for i in range(len(s)):
-            # odd palindrome
-            l, r = i, i
+        for c in range(len(s)):
+            # odd length palindromes
+            l, r = c, c
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > longest:
-                    longest = r  - l + 1
-                    res = s[l : r + 1]
+                if (r - l + 1) > res_len:
+                    res = s[l: r + 1]
+                    res_len = r - l + 1
                 l -= 1
                 r += 1
-
-            # even palindrome
-            l, r = i, i + 1
+            
+            # even length palindromes
+            l, r = c, c + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
-                if (r - l + 1) > longest:
-                    longest = r  - l + 1
-                    res = s[l : r + 1]
+                if (r - l + 1) > res_len:
+                    res = s[l: r + 1]
+                    res_len = r - l + 1
                 l -= 1
                 r += 1
-        
+            
         return res
